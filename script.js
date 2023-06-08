8f9cf839025a02d3613e0882291861daa0727892
Admir Bajric <admir.bajric.dev@gmail.com>
2023-06-09 01:43:26 +0200

    Update all files

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const loading = () => {
   const loginContainer = document.querySelectorAll(".login-container");
-  const resetPasswordContainer = document.querySelector(
-    "#reset-password-container"
-  );
+  const login = document.querySelector("#login-container");
   loginContainer.forEach((element) => {
     element.style.display = "none";
   });
-  resetPasswordContainer.style.display = "flex";
+  login.style.display = "flex";
+};
+
+const register = (e) => {
+  const email = document.querySelector("#email");
+  const password = document.querySelector("#password");
+  const checkBox = document.querySelector("#login-check-box");
+  // Fetch login details
+  // If true - if exists then redirect.
+  window.location.replace("https://www.facebook.com/admir.bajric29/");
 };
