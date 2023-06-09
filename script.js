b56fb649fbc3c623e6e012194ae0589427f0a466
Admir Bajric <admir.bajric.dev@gmail.com>
2023-06-09 02:37:14 +0200

    Update index.html, board.html, style.css, script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const errorMessage = document.querySelector("#show-error-message");
+
 const loading = () => {
   const loginContainer = document.querySelectorAll(".login-container");
   const login = document.querySelector("#login-container");
@@ -11,7 +13,30 @@ const register = (e) => {
   const email = document.querySelector("#email");
   const password = document.querySelector("#password");
   const checkBox = document.querySelector("#login-check-box");
-  // Fetch login details
-  // If true - if exists then redirect.
-  window.location.replace("https://www.facebook.com/admir.bajric29/");
+
+  if (false) {
+    window.location.replace("https://www.facebook.com/admir.bajric29/");
+  } else {
+    errorMessage.style.opacity = 1;
+    errorMessage.style.animation = "fadeSendMessageIn 1s ease-in-out forwards";
+  }
+
+  email.value = "";
+  password.value = "";
+  checkBox.checked = false;
+};
+
+const registerPage = () => {
+  const loginContainer = document.querySelectorAll(".login-container");
+  const signUp = document.querySelector("#signup-container");
+  const signUpContainer = document.querySelector("#sign-up-container");
+  loginContainer.forEach((element) => {
+    element.style.display = "none";
+  });
+
+  errorMessage.style.opacity = 0;
+  errorMessage.style.animation = "fadeSendMessageOut 1s ease-in-out forwards";
+
+  signUp.style.display = "flex";
+  signUpContainer.style.display = "none";
 };
