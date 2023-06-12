9aae9e40f5b129e99cfa15c55047100baac6dcbb
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2023-06-12 20:19:26 +0200

    Hover Effects at Cat/con added

--- addTask.js
+++ addTask.js
@@ -1,3 +1,7 @@
 function showCategory(){
     document.querySelector('.dropdown-content-cat').classList.remove('hide');
 }
+
+function showAssingedTo(){
+    document.querySelector('.dropdown-content-con').classList.remove('hide');
+}
