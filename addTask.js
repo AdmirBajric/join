19cc6cbacd9998d8f11c81532a7ef3cff3d3cffd
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2023-06-12 21:06:58 +0200

    .

--- addTask.js
+++ addTask.js
@@ -1,20 +1,38 @@
-    const ddCategory = document.querySelector('.dropdown-content-cat');
-    const showCategory = document.getElementById('showCategory');
+const ddCategory = document.querySelector('.dropdown-content-cat');
+const showCategory = document.getElementById('showCategory');
+const showAssingedTo = document.getElementById('showAssignedTo');
+const ddContact = document.querySelector('.dropdown-content-con');
+const urgentButton = document.getElementById('urgent');
+const mediumButton = document.getElementById('medium');
+const lowButton = document.getElementById('low');
 
-    function toggleDropdownCat() {
-        ddCategory.classList.toggle('hide');
-        ddCategory.classList.toggle('animate-dropdown');
-    }
+function toggleDropdownCat() {
+    ddCategory.classList.toggle('hide');
+    ddCategory.classList.toggle('animate-dropdown');
+}
 
-    showCategory.addEventListener('click', toggleDropdownCat);
+showCategory.addEventListener('click', toggleDropdownCat);
 
+function toggleDropdownCon(){
+    ddContact.classList.toggle('hide');
+    ddContact.classList.toggle('animate-dropdown')
+}
+showAssingedTo.addEventListener('click', toggleDropdownCon);
 
+urgentButton.addEventListener('click', function() {
+urgentButton.style.backgroundColor = 'var(--development)';
+mediumButton.style.backgroundColor = '';
+lowButton.style.backgroundColor = '';
+});
 
-    const showAssingedTo = document.getElementById('showAssignedTo');
-    const ddContact = document.querySelector('.dropdown-content-con');
+mediumButton.addEventListener('click', function() {
+urgentButton.style.backgroundColor = '';
+mediumButton.style.backgroundColor = 'var(--contactFour)';
+lowButton.style.backgroundColor = '';
+});
 
-    function toggleDropdownCon(){
-        ddContact.classList.toggle('hide');
-        ddContact.classList.toggle('animate-dropdown')
-    }
-    showAssingedTo.addEventListener('click', toggleDropdownCon);
+lowButton.addEventListener('click', function() {
+urgentButton.style.backgroundColor = '';
+mediumButton.style.backgroundColor = '';
+lowButton.style.backgroundColor = 'var(--contactThree)';
+});
