26a1d3c1bcb6fa909d39769d4122f6037d4edab7
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2023-06-12 21:12:11 +0200

    AddTask Board

---
+++ addTask.js
@@ -0,0 +1,38 @@
+const ddCategory = document.querySelector('.dropdown-content-cat');
+const showCategory = document.getElementById('showCategory');
+const showAssingedTo = document.getElementById('showAssignedTo');
+const ddContact = document.querySelector('.dropdown-content-con');
+const urgentButton = document.getElementById('urgent');
+const mediumButton = document.getElementById('medium');
+const lowButton = document.getElementById('low');
+
+function toggleDropdownCat() {
+    ddCategory.classList.toggle('hide');
+    ddCategory.classList.toggle('animate-dropdown');
+}
+
+showCategory.addEventListener('click', toggleDropdownCat);
+
+function toggleDropdownCon(){
+    ddContact.classList.toggle('hide');
+    ddContact.classList.toggle('animate-dropdown')
+}
+showAssingedTo.addEventListener('click', toggleDropdownCon);
+
+urgentButton.addEventListener('click', function() {
+urgentButton.style.backgroundColor = 'var(--development)';
+mediumButton.style.backgroundColor = '';
+lowButton.style.backgroundColor = '';
+});
+
+mediumButton.addEventListener('click', function() {
+urgentButton.style.backgroundColor = '';
+mediumButton.style.backgroundColor = 'var(--contactFour)';
+lowButton.style.backgroundColor = '';
+});
+
+lowButton.addEventListener('click', function() {
+urgentButton.style.backgroundColor = '';
+mediumButton.style.backgroundColor = '';
+lowButton.style.backgroundColor = 'var(--contactThree)';
+});
