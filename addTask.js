e5afaa89adb848a40aea3823d6bafde10b6cebe6
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2023-06-13 16:05:08 +0200

    Subtask add

--- addTask.js
+++ addTask.js
@@ -5,8 +5,8 @@ const ddContact = document.querySelector('.dropdown-content-con');
 const urgentButton = document.getElementById('urgent');
 const mediumButton = document.getElementById('medium');
 const lowButton = document.getElementById('low');
-const salesLink = document.getElementById('sales');
-const backOfficeLink = document.getElementById('backOffice');
+
+let newAddedSubtasks = [];
 
 function toggleDropdownCat() {
     ddCategory.classList.toggle('hide');
@@ -20,9 +20,6 @@ function toggleDropdownCon(){
     ddContact.classList.toggle('animate-dropdown')
 }
 showAssingedTo.addEventListener('click', toggleDropdownCon);
-
-
-
 
 function urgentButtonColor(){
     urgentButton.addEventListener('click', function() {
@@ -51,17 +48,130 @@ function lowButtonColor(){
 }
 lowButtonColor();
 
-// Add event listeners to the links
-salesLink.addEventListener('click', selectCategory);
-backOfficeLink.addEventListener('click', selectCategory);
+function addSubtasks() {
+    let newSubtasks = document.getElementById('new-subtask');
+    let displayAddedSubtask = document.getElementById('added-subtasks');
+    newAddedSubtasks.push(newSubtasks.value);
+    console.log(newAddedSubtasks);
 
-// Function to handle the click event
-function selectCategory(event) {
-    event.preventDefault(); // Prevent the default link behavior
+    // Clear the display area before adding the subtasks
+    displayAddedSubtask.innerHTML = '';
 
-    const selectedCategory = document.getElementById('selectedCategory');
-
-    // Update the selected category text
-    selectedCategory.textContent = event.target.textContent;
+    for (let i = 0; i < newAddedSubtasks.length; i++) {
+        displayAddedSubtask.innerHTML += addSubTaskTempHtml(i);        
     }
+    newSubtasks.value = '';
+}
 
+
+function addSubTaskTempHtml(i){
+    return`
+    <div class="sub-container-task">
+        <input type="checkbox" name="Subtask ${i + 1}">
+        <p>${newAddedSubtasks[i]}</p>
+    </div>
+    `
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function selectCategory(category, color) {
+    //     var selectedCategory = document.getElementById("selectedCategory");
+    //     var dot = document.createElement("span");
+    //     dot.className = "dot " + color;
+        
+    //     selectedCategory.innerHTML = "";
+    //     // Set the category text
+    //     selectedCategory.textContent = category;
+    //     // Append the dot element
+    //     selectedCategory.appendChild(dot);
+    //     var dropdownContent = document.querySelector(".dropdown-content-cat");
+    //     dropdownContent.classList.add("hide");
+    //     }
+    //     // Event listener for Sales category
+    // var salesCategory = document.getElementById("sales");
+    //     salesCategory.addEventListener("click", function(event) {
+    //     event.preventDefault(); // Prevent the default link behavior
+    //     selectCategory("Sales", "dot-red");
+    // });
+    // // Event listener for Backoffice category
+    // var backofficeCategory = document.getElementById("backOffice");
+    //     backofficeCategory.addEventListener("click", function(event) {
+    //     event.preventDefault(); // Prevent the default link behavior
+    //     selectCategory("Backoffice", "dot-pink");
+    // });
+    
+    // // JavaScript function to handle contact selection and close the dropdown
+    // function selectContacts() {
+    //     var selectedContacts = [];
+    //     var checkboxes = document.querySelectorAll(".dropdown-content-con input[type='checkbox']");
+        
+    //     // Loop through the checkboxes
+    //     checkboxes.forEach(function(checkbox) {
+    //         if (checkbox.checked) {
+    //         selectedContacts.push(checkbox.parentNode.textContent.trim());
+    //         }
+    //     });
+    
+    //     var selectedContactsElement = document.getElementById("selected-contacts");
+    //     selectedContactsElement.textContent = "Selected contacts: " + selectedContacts.join(", ");
+    
+    //     // Hide the dropdown content
+    //     var dropdownContent = document.querySelector(".dropdown-content-con");
+    //     dropdownContent.classList.add("hide");
+    //     }
+    
+    //     // Event listeners for contact checkboxes
+    //     var checkboxes = document.querySelectorAll(".dropdown-content-con input[type='checkbox']");
+    //     checkboxes.forEach(function(checkbox) {
+    //     checkbox.addEventListener("change", function() {
+    //         selectContacts();
+    //     });
+    //     });
