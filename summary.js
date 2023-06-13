009234bee6f7765833de8409e6929e1fb2a6f594
Emanuel Schäfer <emanuel-schaefer1@web.de>
2023-06-13 10:09:04 -0400

    %*

--- summary.js
+++ summary.js
@@ -1,3 +1,24 @@
 function toBoard() {
     window.location.href = "board.html";
 }
+
+/*sidebar navigation*/
+function gotoSummary() {
+    window.location.href = "summary.html";
+}
+
+function gotoBoard() {
+    window.location.href = "board.html";
+}
+
+function gotoAddTask() {
+    window.location.href = "add_task.html";
+}
+
+function gotoContacts() {
+    window.location.href = "contacts.html";
+}
+
+function gotoLegalNotice() {
+    window.location.href = "legal_notice.html";
+}
