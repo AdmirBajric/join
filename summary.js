76fb0f845ff82d05d4d2b35dd63b97866536a787
Emanuel Schäfer <emanuel-schaefer1@web.de>
2023-06-14 10:47:55 -0400

    %*

--- summary.js
+++ summary.js
@@ -2,6 +2,20 @@ function toBoard() {
     window.location.href = "board.html";
 }
 
+/*header log out*/
+function Logout() {
+    if(document.getElementById('log-img').classList.contains('d-none')) {
+        document.getElementById('log-img').classList.remove('d-none')
+    }else{
+        document.getElementById('log-img').classList.add('d-none')
+    }
+}
+
+function Logoutout() {
+  //  localStorage.removeItem("");
+    window.location.href = "index.html";
+}
+
 /*sidebar navigation*/
 function gotoSummary() {
     window.location.href = "summary.html";
