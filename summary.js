2cf256d88b1168ed0e7668be35ec033fbc7b576a
Emanuel Schäfer <emanuel-schaefer1@web.de>
2023-06-14 16:19:20 -0400

    %*

--- summary.js
+++ summary.js
@@ -1,3 +1,24 @@
+async function load() {
+    await includeHTML();
+}
+
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+// linked in all directions from here
+
 function toBoard() {
     window.location.href = "board.html";
 }
