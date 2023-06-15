652ff97d51b7325e1a6edc2d2b49d6348b3cc764
Emanuel Schäfer <emanuel-schaefer1@web.de>
2023-06-15 16:17:22 -0400

    %*

--- summary.js
+++ summary.js
@@ -1,7 +1,12 @@
 async function load() {
     await includeHTML();
+    await greetingDay();
+    dateContainer();
 }
 
+
+
+// includeHTML
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -16,11 +21,45 @@ async function includeHTML() {
     }
 }
 
-// linked in all directions from here
 
-function toBoard() {
-    window.location.href = "board.html";
+
+// greeting day on
+async function greetingDay() {
+    document.getElementById("dayGreeting").innerText = alldayGreeting();
 }
+
+function alldayGreeting() {
+    let hour = new Date().getHours();
+    if (4 <= hour && hour <= 11) {
+      return "Good morning,";
+    }
+    if (11 < hour && hour <= 19) {
+      return "Good afternoon,";
+    }
+    if (19 < hour || hour < 4) {
+      return "Good evening,";
+    }
+  }
+
+
+
+/*date today*/
+function dateContainer() {
+    let containerDay = new Date().toLocaleString();
+    containerDay = containerDay.slice(0, 10);
+    let containerDayArray = containerDay.split(".");
+    let containerMonth = getMonthContainer(containerDayArray[1]);
+    containerDay = containerMonth + " " + containerDayArray[0] + ", " + containerDayArray[2];
+    document.getElementById('dateContainer').innerHTML = `<strong>${containerDay}</strong>`;
+}
+
+function getMonthContainer(i){
+    const date = new Date();
+    date.setMonth(i - 1);
+    return date.toLocaleString('en-us', {month: 'short'});
+  }
+
+
 
 /*header log out*/
 function Logout() {
@@ -31,10 +70,19 @@ function Logout() {
     }
 }
 
+
+
 function Logoutout() {
   //  localStorage.removeItem("");
     window.location.href = "index.html";
 }
+
+
+// linked in all directions from here
+function toBoard() {
+    window.location.href = "board.html";
+}
+
 
 /*sidebar navigation*/
 function gotoSummary() {
