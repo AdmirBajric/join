c560ade121b9cf99cf94d9b3d77df4f736de5813
Admir Bajric <admir.bajric.dev@gmail.com>
2023-06-27 00:24:32 +0200

    Update guestLogIn

--- guestLogIn.js
+++ guestLogIn.js
@@ -1,3 +1,9 @@
 const guestLogin = async (id) => {
-    
+  let users = JSON.parse(await getItem("users"));
+  users.filter((user) => {
+    if (user.id === id) {
+      localStorage.setItem("user", JSON.stringify(user));
+      location.href = "http://127.0.0.1:5500/summary.html";
+    }
+  });
 };
