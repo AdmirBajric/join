d792b29d47d01fe5417df80da90582823add9943
Emanuel Schäfer <emanuel-schaefer1@web.de>
2023-06-28 14:45:21 -0400

    %*

--- log-out.js
+++ log-out.js
@@ -1,13 +1,15 @@
 /*header log out*/
 function Logout() {
-    if(document.getElementById('log-img').classList.contains('d-none')) {
-        document.getElementById('log-img').classList.remove('d-none')
+    if(document.getElementById('mobile-options').classList.contains('d-none')) {
+        document.getElementById('mobile-options').classList.remove('d-none')
     }else{
-        document.getElementById('log-img').classList.add('d-none')
+        document.getElementById('mobile-options').classList.add('d-none')
     }
 }
 
 function Logoutout() {
-  //  localStorage.removeItem("");
+    localStorage.removeItem("user");
+    localStorage.removeItem('userTasks');
+    localStorage.removeItem('userContacts');
     window.location.href = "index.html";
 }
