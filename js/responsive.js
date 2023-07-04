a9c84f781b91b0ae17440d361d56228374a1a46e
Admir Bajric <admir.bajric.dev@gmail.com>
2023-07-04 23:39:58 +0200

    Update files
    Resposive login

--- js/responsive.js
+++ js/responsive.js
@@ -1,9 +1,13 @@
 const responsive = () => {
   const body = document.querySelector("body");
   const joinStartImg = document.querySelector("#join-start-img-responsive");
+  const signUpContainer = document.querySelector("#sign-up-container");
+  const loginContainer = document.querySelector("#login-container");
 
   setTimeout(() => {
     body.style.animation = "changeBcg 1s ease-in-out forwards";
     joinStartImg.style.animation = "changeImg 1s linear forwards";
+    signUpContainer.style.animation = "fadeInResponsive 1s ease-in-out forwards";
+    loginContainer.style.animation = "fadeInResponsive 1s ease-in-out forwards";
   }, 2000);
 };
