120f7d4771cce15c3fd6ecce44a2f05165fbbac2
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2023-07-14 09:12:59 +0200

    commit deleted (issue)

--- js/board_drag_drop.js
+++ js/board_drag_drop.js
@@ -22,23 +22,33 @@ function drag(ev) {
     ev.dataTransfer.setData("text/plain", ev.target.id);
 }
 
-async function drop(ev, status) {
+function drop(ev, status) {
     ev.preventDefault();
     ev.stopPropagation();
     let data = ev.dataTransfer.getData("text/plain");
     let draggedElement = document.getElementById(data);
     let dropArea = ev.target;
-
-    // Check if the drop area is valid for the dragged element
     if (dropArea.classList.contains("drop-area")) {
         dropArea.appendChild(draggedElement);
-        await moveTo(status);
+        moveTo(status);
     } else {
-        // Invalid drop area, return the element to its original position
-        draggedElement.classList.remove("d-none");
+        let originalContainer = document.getElementById(status);
+        originalContainer.appendChild(draggedElement);
+        ev.preventDefault();
+        draggedElement.classList.remove("dragging");
     }
-}
+    getBorderRemoveFunctions();
+    }
 
+
+
+function hideDraggedOrigin() {
+    let element = document.getElementById(currentDraggedElement);
+    if (element) {
+        element.classList.remove("d-none");
+    }
+    }
+    
 async function moveTo(status) {
     let targetArray;
     switch (status) {
